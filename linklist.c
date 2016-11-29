cdb15268c6af589c616051fcde9f520dd1da8491
GaoleiBai <937294944@qq.com>
2016-11-29 10:06:50 +0800

    Update linklist.c

--- linklist.c
+++ linklist.c
@@ -1,1 +1,26 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "d_linklist.h" 
+
+int linkList_create(linklist_pt *list)
+{       
+        linklist_pt kLinkList = (linklist_pt) malloc( sizeof(*kLinkList) );
+        if(kLinkList)
+        {       
+                kLinkList->header = (linklistentry_pt) malloc( sizeof(*(kLinkList->header)) );
+                if(kLinkList->header)
+                {       
+                        kLinkList->header->elem = NULL;
+                        kLinkList->header->next = kLinkList->header;
+                        kLinkList->header->prev = kLinkList->header;
+                        kLinkList->size = 0;
+                        
+                        *list = kLinkList;
+                        
+                        return 0;
+                }
+        }
+        return -1;
+}
