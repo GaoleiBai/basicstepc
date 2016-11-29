e52b7757e8442c538115ba1fd19599c3edfbb370
GaoleiBai <937294944@qq.com>
2016-11-29 18:33:23 +0800

    Update log_inerface.c

--- log_inerface.c
+++ log_inerface.c
@@ -32,3 +32,26 @@ void dispDbgFmt( const zsint8 * format, ... )
         }
 #endif
 }
+/**********************************************************************/
+/** @note display debug information
+ * 
+ * @param               tSysRunMode     (in):   system run mode 
+ * @param               format ...      (in):   format input parameter
+ * @return              void
+ */
+void logSysInfo( zsint32 prio, const zsint8 * format, ... )
+{
+        va_list arg_ptr;
+
+        va_start(arg_ptr, format);
+
+#if ! defined( ENABLE_DBG_PRINT )
+        if ( FALSE == SFW_DEBUG_EN )
+                syslog( prio, format, arg_ptr );
+#else
+        if ( TRUE == SFW_DEBUG_EN )
+                vfprintf( stderr, format, arg_ptr );
+#endif
+
+        va_end(arg_ptr);
+}
