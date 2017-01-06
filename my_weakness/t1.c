5e07e8ec9a61e889323ca8d314fa43d973faac50
GaoleiBai <937294944@qq.com>
2017-01-06 15:17:46 +0800

    Update t1.c

--- my_weakness/t1.c
+++ my_weakness/t1.c
@@ -1,1 +1,6 @@
+static struct student st[] ={{.name = "zhangsan",.age = 10 }
+                             ,{.name = "lisi" ,.age = 11}
+                             ,{.name = "wangwu" , .age = 30}
+                            };
 
+void (*fun[]0（int ， int ）
